a1bc65109e807688d041e401986f4762cda6879f
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-10-27 11:16:17 +0800

    u

--- Ch01_DNN_classification/Ex01B/loader.py
+++ Ch01_DNN_classification/Ex01B/loader.py
@@ -11,6 +11,25 @@ def load_data_1d(filename):
         data = asarray(data).astype(float)
 
     x = data[:, 0] # input data
-    y = data[:, 1] # output data / targets / ground truth
+    y = data[:, 1] # output data / targets / groundtruth
+    
+    return (x, y)
+
+def load_data(filename):
+    import csv
+    from numpy import asarray, reshape
+    
+    with open(filename) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        data = []
+        for row in reader:
+            data.append(row)
+        data = asarray(data).astype(float)
+
+    x = data[:, 0:-1] # input data
+
+    num_samples = data.shape[0]
+    y = reshape(data[:, -1], (num_samples, 1)) # output data/targets/groundtruth
     
     return (x, y)
