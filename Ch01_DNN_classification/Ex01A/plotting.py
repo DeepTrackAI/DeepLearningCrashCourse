c64d992d143033272273d146da621133fdbefae4
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-10-27 10:44:58 +0800

    Ex01A

---
+++ Ch01_DNN_classification/Ex01A/plotting.py
@@ -0,0 +1,22 @@
+def plot_data_1d(x, y_gt):
+    import matplotlib.pyplot as plt
+
+    plt.scatter(x, y_gt, s=20, c="k")
+    plt.axis("equal")
+    plt.xlabel("x0", fontsize=24)
+    plt.ylabel("y", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
+
+
+def plot_pred_1d(x, y_gt, y_p):
+    import matplotlib.pyplot as plt
+
+    plt.scatter(x, y_gt, s=20, c="k", label="ground truth")
+    plt.scatter(x, y_p, s=100, c="tab:orange", marker="x", label="predicted")
+    plt.legend(fontsize=20)
+    plt.axis("equal")
+    plt.xlabel("x0", fontsize=24)
+    plt.ylabel("y", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
