39b37c0251d9c26a0688fb3c0b0dd58c487a8610
Carlo <carlo.manzo@gmail.com>
2023-11-24 16:01:00 +0100

    octaves

--- Ch03_CNN/Ex03E/deepdream.py
+++ Ch03_CNN/Ex03E/deepdream.py
@@ -13,8 +13,7 @@ class fwd_hook():
 def preprocess(image):
     import torch 
     from torchvision import transforms
-    normalize = transforms.Compose([transforms.Resize(256),
-                            transforms.ToTensor(),
+    normalize = transforms.Compose([transforms.ToTensor(),
                             transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]),
                             ])
