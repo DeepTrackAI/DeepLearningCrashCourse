9c9e94e8d371c2a4fd9f00c6e368dd86a0dfffda
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-11-25 17:24:19 +0100

    Update plotting_cnn.py

--- Ch03_CNN/Ex03A/plotting_cnn.py
+++ Ch03_CNN/Ex03A/plotting_cnn.py
@@ -3,25 +3,26 @@ def plot_image(image):
     
     plt.imshow(image, cmap="gray", aspect="equal", 
                extent=[0, image.shape[1], 0, image.shape[0]])
-    plt.grid(color="red", linewidth=1)
+    plt.colorbar()
     plt.xticks(range(0, image.shape[1] + 1))
     plt.yticks(range(0, image.shape[0] + 1))
+    plt.grid(color="red", linewidth=1)
     plt.tight_layout()
     plt.show()
 
 def plot_channels(channels):
     import matplotlib.pyplot as plt
 
-    fig, axs = plt.subplots(1, channels.shape[0])
+    fig, axs = plt.subplots(1, channels.shape[0], figsize=(12, 4))
 
     for channel, ax, i in zip(channels, axs, range(channels.shape[0])):
-        ax.imshow(channel, cmap="gray", aspect="equal", 
-                  extent=[0, channel.shape[1], 0, channel.shape[0]])
-        ax.grid(color="red", linewidth=1)
+        im = ax.imshow(channel, cmap="gray", aspect="equal", 
+                       extent=[0, channel.shape[1], 0, channel.shape[0]])
+        plt.colorbar(im)
         ax.set_title(f"Channel {i}")
         ax.set_xticks(range(0, channel.shape[1] + 1))
         ax.set_yticks(range(0, channel.shape[0] + 1))
+        ax.grid(color="red", linewidth=1)
 
     plt.tight_layout()
     plt.show()
-
