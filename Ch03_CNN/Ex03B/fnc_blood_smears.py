dde5ba3a8a940ce464f99067ac4868ee8ae3b0c8
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-12-09 18:44:44 +0100

    u

--- Ch03_CNN/Ex03B/fnc_blood_smears.py
+++ Ch03_CNN/Ex03B/fnc_blood_smears.py
@@ -1,55 +1,49 @@
-def plot_blood_smears(title, files):
+def plot_blood_smears(dataset):
     import matplotlib.pyplot as plt
+    from numpy.random import randint
+    import torch
 
-    fig, axs = plt.subplots(3, 3, figsize=(8, 8))
-    for i, ax in enumerate(axs.ravel()):
-        image = plt.imread(files[i])
+    fig, axs = plt.subplots(3, 6, figsize=(16, 8))
+    for ax in axs.ravel():
+        image, label = dataset[randint(0, len(dataset))]
+        if isinstance(image, torch.Tensor):
+            image = image.numpy().transpose(1, 2, 0)
+            
         ax.imshow(image)
-        
-    fig.suptitle(title, fontsize=16)
+        ax.set_title("Uninfected (1)" if label == 1 else "Infected (0)")
+
+    plt.tight_layout()
     plt.show()
 
 
-def plot_roc(classifier, dataset):
-    from torch import tensor, stack
-    from sklearn.metrics import roc_curve, auc
+def plot_roc(classifier, loader):
+    import torchmetrics as tm
+
+    roc = tm.ROC(task="binary")
+
+    for image, label in loader:
+        roc.update(classifier(image), label.long())
+
+    fig, ax = roc.plot(score=True)
+    ax.grid(False)
+    ax.axis("square")
+    ax.set_xlim(0, 1)
+    ax.set_ylim(0, 1)
+    ax.legend(loc="center right")
+
+
+def plot_failures(images, labels, pred, threshold=0.5, plot_num=5):
     from matplotlib import pyplot as plt
-
-    # calculate predictions
-    images, gt = zip(*dataset)
-    pred = classifier(tensor(stack(images))).tolist()
-    
-    # calculate the ROC curve
-    fpr, tpr, thresholds = roc_curve(gt, pred, pos_label=1) 
-    roc = auc(fpr, tpr) 
-
-    # plot the ROC curve
-    plt.figure(figsize=(5, 5))
-    plt.plot(fpr, tpr, label=f"ROC curve (AUC = {roc:.3f})", linewidth=2)
-    plt.title("ROC curve")
-    plt.xlabel("False positive rate")
-    plt.ylabel("True positive rate")
-    plt.axis("square")
-    plt.xlim(0, 1)
-    plt.ylim(0, 1)
-    plt.legend(loc = 'center right')
-    plt.show()
-
-    return images, gt, pred, roc
-
-
-def plot_failures(images, gt, pred, threshold=.5, plot_num=5):
-    from matplotlib import pyplot as plt 
     from numpy import array, squeeze
-    
+
     pred = array(pred).squeeze()
-    gt = array(gt).squeeze()
+    labels = array(labels).squeeze()
     images = array(images)
 
-    false_positives = (pred > threshold) & (gt == 0)
+    false_positives = (pred > threshold) & (labels == 0)
     false_positives_images = images[false_positives]
 
-    false_negatives = (pred < threshold) & (gt == 1)
+    false_negatives = (pred < threshold) & (labels == 1)
     false_negatives_images = images[false_negatives]
 
     plt.figure(figsize=(plot_num * 2, 5))
@@ -70,23 +64,23 @@ def plot_failures(images, gt, pred, threshold=.5, plot_num=5):
     plt.show()
 
 
-class fwd_hook():
+class fwd_hook:
     def __init__(self, layer):
         self.hook = layer.register_forward_hook(self.hook_func)
 
     def hook_func(self, layer, i, o):
-        print("Forward hook running ...") 
+        print("Forward hook running ...")
         self.activations = o.detach().clone()
         print(f"Activations size: {self.activations.size()}")
 
-    def __enter__(self, *args): 
+    def __enter__(self, *args):
         return self
-    
-    def __exit__(self, *args): 
+
+    def __exit__(self, *args):
         self.hook.remove()
 
 
-class bwd_hook():
+class bwd_hook:
     def __init__(self, layer):
         self.hook = layer.register_full_backward_hook(self.hook_func)
 
@@ -95,10 +89,10 @@ class bwd_hook():
         self.gradients = go[0].detach().clone()
         print(f"Gradients size: {self.gradients.size()}")
 
-    def __enter__(self, *args): 
+    def __enter__(self, *args):
         return self
-    
-    def __exit__(self, *args): 
+
+    def __exit__(self, *args):
         self.hook.remove()
 
 
@@ -109,7 +103,7 @@ def plot_activations(activations, cols=8):
 
     fig, axs = plt.subplots(rows, cols, figsize=(2 * cols, 2 * rows))
     for i, ax in enumerate(axs.ravel()):
-        ax.axis('off')
+        ax.axis("off")
         if i < activations.shape[0]:
             ax.imshow(activations[i].numpy())
             ax.set_title(i)
@@ -118,29 +112,31 @@ def plot_activations(activations, cols=8):
     plt.show()
 
 
-def plot_gradcam(image, grad_cam):
+def plot_heatmap(image, heatmap):
     from matplotlib import pyplot as plt
     import skimage
+    from numpy import array
 
-    grad_cam = skimage.transform.resize(grad_cam, image.shape, order=2)
-    grad_cam = skimage.exposure.rescale_intensity(grad_cam, out_range=(0.25, 1))
+    image = skimage.exposure.rescale_intensity(array(image), out_range=(0, 1))
+    heatmap = skimage.transform.resize(heatmap, image.shape, order=2)
+    heatmap = skimage.exposure.rescale_intensity(heatmap, out_range=(0.25, 1))
 
-    plt.figure(figsize=(12, 5)) 
+    plt.figure(figsize=(12, 5))
 
     plt.subplot(1, 3, 1)
     plt.imshow(image, interpolation="bilinear")
-    plt.title("Original image")
+    plt.title("Original image", fontsize=16)
     plt.axis("off")
-    
+
     plt.subplot(1, 3, 2)
-    plt.imshow(grad_cam.mean(axis=-1), interpolation="bilinear")
-    plt.title("Grad-CAM")
+    plt.imshow(heatmap.mean(axis=-1), interpolation="bilinear")
+    plt.title("Heatmap with Grad-CAM", fontsize=16)
     plt.axis("off")
-    
+
     plt.subplot(1, 3, 3)
-    plt.imshow(image * grad_cam)
-    plt.title("Overlay")
+    plt.imshow(image * heatmap)
+    plt.title("Overlay", fontsize=16)
     plt.axis("off")
-    
+
     plt.tight_layout()
     plt.show()
