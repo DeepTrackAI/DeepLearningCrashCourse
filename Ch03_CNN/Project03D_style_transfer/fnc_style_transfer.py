d8244c89d6cb82b04bbd11fdff6ae3c645ee6b87
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2023-12-10 09:32:11 +0100

    u

--- Ch03_CNN/Project03D_style_transfer/fnc_style_transfer.py
+++ Ch03_CNN/Project03D_style_transfer/fnc_style_transfer.py
@@ -1,16 +1,35 @@
+def image_to_tensor(im, mean, std):
+    import torchvision.transforms as tt
+
+    normalize = tt.Compose([tt.ToTensor(), tt.Normalize(mean, std)])
+
+    return normalize(im).unsqueeze(0).requires_grad_(True)
+
+
+def tensor_to_image(image, mean, std):
+    import torchvision.transforms as tt
+    import numpy as np
+    from PIL import Image
+
+    denormalize = tt.Normalize(mean=-mean / std, std=1 / std)
+
+    im_array = denormalize(image.data.clone().detach().squeeze()).numpy()
+    im_array = np.clip(im_array.transpose(1, 2, 0) * 255, 0, 255).astype(np.uint8)
+    return Image.fromarray(im_array, 'RGB')
 
 class fwd_hooks():
-    def __init__(self, ms):
-        self.ms = ms
-        self.hook, self.stored = [], []
-        for m in self.ms:
-            self.hook.append(m.register_forward_hook(self.hook_func))
-    def hook_func(self, m, i, o):
-        # print('Forward hook running...') 
-        self.stored.append(o)
-        # print(f'Activations size: {o.size()}')
+    def __init__(self, layers):
+        self.hooks = []
+        self.activations_list = []
+        for layer in layers:
+            self.hooks.append(layer.register_forward_hook(self.hook_func))
+
+    def hook_func(self, layer, input, output):
+        self.activations_list.append(output)
+
     def __enter__(self, *args): 
-         return self
+        return self
+    
     def __exit__(self, *args): 
-        for h in self.hook:    
-            h.remove()
+        for hook in self.hooks:
+            hook.remove()
