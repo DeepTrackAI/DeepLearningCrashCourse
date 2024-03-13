cf34d1d7ca63fb18e4d4cb5a311ac2822bdabd50
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-03-13 17:34:50 +0800

    u

--- Ch01_DNN_classification/ec01_2_neuron_clas_2d/plotting.py
+++ Ch01_DNN_classification/ec01_2_neuron_clas_2d/plotting.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 def plot_data_1d(x, y_gt):
     """Plot 1D data."""
     plt.scatter(x, y_gt, s=20, c="k")
-    plt.axis("equal")
     plt.xlabel("x0", fontsize=24)
     plt.ylabel("y", fontsize=24)
     plt.tick_params(axis="both", which="major", labelsize=16)
@@ -17,7 +16,6 @@ def plot_pred_1d(x, y_gt, y_p):
     plt.scatter(x, y_gt, s=20, c="k", label="ground truth")
     plt.scatter(x, y_p, s=100, c="tab:orange", marker="x", label="predicted") 
     plt.legend(fontsize=20)
-    plt.axis("equal")
     plt.xlabel("x0", fontsize=24)
     plt.ylabel("y", fontsize=24)
     plt.tick_params(axis="both", which="major", labelsize=16)
