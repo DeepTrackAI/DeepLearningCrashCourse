6ce14fb356c213009ad8b5a39423ca00e587a482
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-03-13 15:58:12 +0800

    Create plotting.py

---
+++ Ch01_DNN_classification/ec01_2_neuron_clas_2d/plotting.py
@@ -0,0 +1,35 @@
+"""Module with functions to plot data."""
+import matplotlib.pyplot as plt
+
+
+def plot_data_1d(x, y_gt):
+    """Plot 1D data."""
+    plt.scatter(x, y_gt, s=20, c="k")
+    plt.axis("equal")
+    plt.xlabel("x0", fontsize=24)
+    plt.ylabel("y", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
+
+
+def plot_pred_1d(x, y_gt, y_p):
+    """Plot 1D data and predictions."""
+    plt.scatter(x, y_gt, s=20, c="k", label="ground truth")
+    plt.scatter(x, y_p, s=100, c="tab:orange", marker="x", label="predicted") 
+    plt.legend(fontsize=20)
+    plt.axis("equal")
+    plt.xlabel("x0", fontsize=24)
+    plt.ylabel("y", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
+
+
+def plot_data_2d(x, y_gt):
+    """Plot 2D data."""
+    plt.scatter(x[:, 0], x[:, 1], c=y_gt, s=50)
+    plt.colorbar()
+    plt.axis("equal")
+    plt.xlabel("x0", fontsize=24)
+    plt.ylabel("x1", fontsize=24) 
+    plt.tick_params(axis="both", which="major", labelsize=16) 
+    plt.show()
