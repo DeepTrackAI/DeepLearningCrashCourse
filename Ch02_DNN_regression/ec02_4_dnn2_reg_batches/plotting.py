71dd524e204a246c3f879e3cd3cd7428202ce6af
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-03-19 18:09:23 +0100

    u

--- Ch02_DNN_regression/ec02_4_dnn2_reg_batches/plotting.py
+++ Ch02_DNN_regression/ec02_4_dnn2_reg_batches/plotting.py
@@ -1,6 +1,6 @@
 """Module with functions to plot data."""
 import matplotlib.pyplot as plt
-
+from numpy import convolve, full
 
 def plot_data_1d(x, y_gt):
     """Plot 1D data."""
@@ -43,4 +43,38 @@ def plot_pred_2d(x, y_gt, y_p):
     plt.xlabel("x0", fontsize=24)
     plt.ylabel("x1", fontsize=24) 
     plt.tick_params(axis="both", which="major", labelsize=16) 
+    plt.show()
+
+
+def plot_pred_vs_gt(y_gt, y_p):
+    """Plot prediction versus ground truth values."""
+    plt.plot([-1, 1], [-1, 1], "k:")
+    plt.scatter(y_gt, y_p, s=10)
+    plt.axis("square")
+    plt.xlabel("y ground truth", fontsize=24)
+    plt.ylabel("y predicted", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
+    plt.show()
+
+
+def plot_mse(mse, smooth=11):
+    """Plot MSE evolution during training."""
+    mse_smooth = convolve(mse, full((smooth,), 1 / smooth), mode="valid")
+    
+    fig, ax = plt.subplots(1, 2)
+    fig.set_size_inches(10, 5)
+    
+    ax[0].plot(mse, c="tab:orange")
+    ax[0].plot(range(smooth // 2, len(mse) - smooth // 2), mse_smooth, c="k")
+    ax[0].set_xlabel("epoch", fontsize=24)
+    ax[0].set_ylabel("MSE", fontsize=24)
+    ax[0].tick_params(axis="both", which="major", labelsize=16)
+
+    ax[1].loglog(mse, c="tab:orange")
+    ax[1].loglog(range(smooth // 2, len(mse) - smooth // 2), mse_smooth, c="k")
+    ax[1].set_xlabel("epoch", fontsize=24)
+    ax[1].set_ylabel("MSE", fontsize=24)
+    ax[1].tick_params(axis="both", which="major", labelsize=16)
+    
+    plt.tight_layout()
     plt.show()
