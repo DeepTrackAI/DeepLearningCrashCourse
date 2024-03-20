9ad1ee2fc6a1d1546b4d35c2f2560e62fdbc9419
Giovanni Volpe <46021832+giovannivolpe@users.noreply.github.com>
2024-03-20 18:58:00 +0100

    u

--- Ch02_DNN_regression/ec02_5_dnn2_reg_split/fig02_16_17_18__/plotting.py
+++ Ch02_DNN_regression/ec02_5_dnn2_reg_split/fig02_16_17_18__/plotting.py
@@ -33,7 +33,7 @@ def plot_data_2d(x, y_gt):
     plt.show()
 
 
-def plot_pred_2d(x, y_gt, y_p):
+def plot_pred_2d(x, y_gt, y_p, fig_name):  ### def plot_pred_2d(x, y_gt, y_p):
     """Plot 2D data and predictions."""
     plt.scatter(x[:, 0], x[:, 1], c=y_gt, s=50, label="ground truth")
     plt.scatter(x[:, 0], x[:, 1], c=y_p, s=100, marker="x", label="predicted") 
@@ -43,10 +43,10 @@ def plot_pred_2d(x, y_gt, y_p):
     plt.xlabel("x0", fontsize=24)
     plt.ylabel("x1", fontsize=24) 
     plt.tick_params(axis="both", which="major", labelsize=16) 
-    plt.show()
+    plt.savefig(fig_name, bbox_inches="tight")  ### plt.show()
 
 
-def plot_pred_vs_gt(y_gt, y_p):
+def plot_pred_vs_gt(y_gt, y_p, fig_name):  ### def plot_pred_vs_gt(y_gt, y_p):
     """Plot prediction versus ground truth values."""
     plt.plot([-1, 1], [-1, 1], "k:")
     plt.scatter(y_gt, y_p, s=10)
@@ -54,7 +54,7 @@ def plot_pred_vs_gt(y_gt, y_p):
     plt.xlabel("y ground truth", fontsize=24)
     plt.ylabel("y predicted", fontsize=24)
     plt.tick_params(axis="both", which="major", labelsize=16)
-    plt.show()
+    plt.savefig(fig_name, bbox_inches="tight")  ### plt.show()
 
 
 def plot_mse(mse, smooth=11):
@@ -80,7 +80,7 @@ def plot_mse(mse, smooth=11):
     plt.show()
 
 
-def plot_mse_train_vs_val(mse_t, mse_v, smooth=11):
+def plot_mse_train_vs_val(mse_t, mse_v, smooth=11, fig_name=""):  ### def plot_mse_train_vs_val(mse_t, mse_v, smooth=11):
     """Plot training and validation MSE evolution during training."""
     mse_t_s = convolve(mse_t, full((smooth,), 1 / smooth), mode="valid")
     mse_v_s = convolve(mse_v, full((smooth,), 1 / smooth), mode="valid")
@@ -105,4 +105,4 @@ def plot_mse_train_vs_val(mse_t, mse_v, smooth=11):
     ax[1].tick_params(axis="both", which="major", labelsize=16)
     
     plt.tight_layout()
-    plt.show()
+    plt.savefig(fig_name, bbox_inches="tight")  ### plt.show()
