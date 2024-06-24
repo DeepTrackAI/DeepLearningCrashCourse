61e72f6b60a53fcecf91d8830410f82bba418ae2
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-06-24 23:39:41 +0800

    Update plotting.py

--- Ch01_DNN_classification/ec01_2_neuron_clas_2d/plotting.py
+++ Ch01_DNN_classification/ec01_2_neuron_clas_2d/plotting.py
@@ -28,15 +28,15 @@ def plot_data_2d(x, y_gt):
     plt.colorbar()
     plt.axis("equal")
     plt.xlabel("x0", fontsize=24)
-    plt.ylabel("x1", fontsize=24) 
-    plt.tick_params(axis="both", which="major", labelsize=16) 
+    plt.ylabel("x1", fontsize=24)
+    plt.tick_params(axis="both", which="major", labelsize=16)
     plt.show()
 
 
 def plot_pred_2d(x, y_gt, y_p):
     """Plot 2D data and predictions."""
     plt.scatter(x[:, 0], x[:, 1], c=y_gt, s=50, label="ground truth")
-    plt.scatter(x[:, 0], x[:, 1], c=y_p, s=100, marker="x", label="predicted") 
+    plt.scatter(x[:, 0], x[:, 1], c=y_p, s=100, marker="x", label="predicted")
     plt.legend(fontsize=20)
     plt.colorbar()
     plt.axis("equal")
