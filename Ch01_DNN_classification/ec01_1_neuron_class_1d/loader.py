09fbbad3a567e6847932c88b19fe14f1567084af
Giovanni Volpe <giovanni.volpe@physics.gu.se>
2024-06-24 23:25:23 +0800

    1.1

--- Ch01_DNN_classification/ec01_1_neuron_class_1d/loader.py
+++ Ch01_DNN_classification/ec01_1_neuron_class_1d/loader.py
@@ -5,13 +5,13 @@ from numpy import asarray
 
 def load_data_1d(filename):
     """Load 1D data."""
-    with open(filename) as f:
-        reader = csv.reader(f)
+    with open(filename) as file:
+        reader = csv.reader(file)
         header = next(reader)
         data = []
         for row in reader:
             data.append(row)
-        data = asarray(data).astype(float)  
+        data = asarray(data).astype(float)
     x = data[:, 0]  # Input.
     y = data[:, 1]  # Output/target/ground truth.
     return (x, y)
--- Ch01_DNN_classification/ec01_1_neuron_class_1d/plotting.py
+++ Ch01_DNN_classification/ec01_1_neuron_class_1d/plotting.py
@@ -14,7 +14,7 @@ def plot_data_1d(x, y_gt):
 def plot_pred_1d(x, y_gt, y_p):
     """Plot 1D data and predictions."""
     plt.scatter(x, y_gt, s=20, c="k", label="ground truth")
-    plt.scatter(x, y_p, s=100, c="tab:orange", marker="x", label="predicted") 
+    plt.scatter(x, y_p, s=100, c="tab:orange", marker="x", label="predicted")
     plt.legend(fontsize=20)
     plt.xlabel("x0", fontsize=24)
     plt.ylabel("y", fontsize=24)
